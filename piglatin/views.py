42e08a8e6eaad8b2c573858cea291a210ae627c0
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-25 11:12:41 +0200

    Added ability to translate to pig latin

--- piglatin/views.py
+++ piglatin/views.py
@@ -5,5 +5,17 @@ def home(request):
 	return render(request, 'home.html')
 
 def translate(request):
-	text_from_form = request.GET['original_text']
-	return HttpResponse("You are on translate page " + text_from_form )
+	text_from_form = request.GET['original_text'].lower()
+
+	translation = ''
+	for word in text_from_form.split():
+		if word[0] in ['a', 'i', 'o', 'e', 'u']:
+			translation += word
+			translation += 'yay '
+		else:
+			translation += word[1:]
+			translation += word[0]
+			translation += 'ay '
+
+
+	return HttpResponse("You are on translate page " + translation )
